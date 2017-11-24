4640c25026cfc34061527e9045d1eb85f411ffb3
Chad Duffey <chadduffey@hotmail.com>
2017-11-24 12:07:12 -0800

    init

---
+++ treehouse_game.py
@@ -0,0 +1,12 @@
+import random
+
+#draw grid
+#pick random location for player
+#pick random location for exit door
+#pick random location for monster
+#draw player in grid
+#take movement input
+#move player unless invalid move past edges of grid
+#check for win/loss
+#clear screen and redraw grid
+
