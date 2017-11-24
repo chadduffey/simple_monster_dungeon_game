a31eda29ee64cd87456f75429d9ce0a8b170e70e
Chad Duffey <chadduffey@hotmail.com>
2017-11-24 12:44:54 -0800

    basic game loop

--- treehouse_game.py
+++ treehouse_game.py
@@ -1,3 +1,4 @@
+import os
 import random
 
 #draw grid
@@ -16,26 +17,64 @@ CELLS = [(0,0), (1,0), (2,0), (3,0), (4,0),
 		 (0,3), (1,3), (2,3), (3,3), (4,3),
 		 (0,4), (1,4), (2,4), (3,4), (4,4)]
 
-def get_locations():
-	monster = None
-	door = None
-	player = None
+def clear_screen():
+	os.system('cls' if os.name == 'nt' else 'clear')
 
-	return monster, door, player
+
+def get_locations():
+	return random.sample(CELLS, 3)
+
+
+def move_player(player, move):
+	x, y = player
+	if move == "LEFT":
+		x -= 1
+	if move == "RIGHT":
+		x += 1
+	if move == "UP":
+		y -= 1
+	if move == "DOWN":
+		y += 1
+	return x, y
+
+
+def get_moves(player):
+	moves = ["LEFT", "RIGHT", "UP", "DOWN"]
+	x, y = player
+	if x == 0:
+		moves.remove("LEFT")
+	if x == 4:
+		moves.remove("RIGHT")
+	if y == 0:
+		moves.remove("UP")
+	if y == 4:
+		moves.remove("DOWN")
+	return moves
+
+monster, door, player = get_locations()
 
 while True:
+	
+	valid_moves = get_moves(player)
+	clear_screen()
+
 	print("Welcome to the dungeon!")
-	print("You are currently in room {}") #fill with player position
-	print("You can move {}") #fill with available moves
+	print("You are currently in room {}".format(player))
+	print("You can move {}".format(", ".join(valid_moves)))
 	print("Enter QUIT to quit")
 
 	move = input("> ")
+	move = move.upper()
 	if move == 'QUIT':
 		break
+	if move in valid_moves:
+		player = move_player(player, move)
+	else:
+		print("\n** Walls are hard! Dont run into them! **\n")
+		continue
 
 	#good move, change player position
 	#bad move dont change
 	#on the monster, they lose
 	#on door, they win
 	#otherwise loop
-	
